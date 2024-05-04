46efc230584fa45063c75e3a56c44ba5364a1882
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-04 17:50:07 -0300

    feat: add initial app

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app
+  .use(cors())
+  .use(express.json())
+  .get("/health", (_req, res) => res.send("I'm healthy!!!"))
+
+export function init() {
+  return Promise.resolve(app);
+}
+
+export async function close(){}
+
+export default app;
