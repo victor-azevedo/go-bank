a6aefc839a13ed9a867795ba3cd0859e1f0d9195
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-04 17:49:10 -0300

    feat: add config envs

---
+++ src/config/envs.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+const envFileMap = {
+  test: ".env.test",
+  development: ".env.development",
+  env: ".env"
+};
+
+type EnvFileMapKeys = keyof typeof envFileMap;
+
+const env = process.env.NODE_ENV;
+
+export function loadEnv() {
+  const defaultEnvFile = ".env";
+  const path = envFileMap.hasOwnProperty(env  as EnvFileMapKeys) ? envFileMap[env  as EnvFileMapKeys] : defaultEnvFile;
+
+  dotenv.config({ path, override: true });
+
+  console.log(`Loaded env: "${path}"`);
+}
