863f73037a9b51f809f411170889924318b6f8e0
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-05 23:34:46 -0300

    feat: add user model

---
+++ src/models/user-model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+export interface User {
+  name: string;
+  cpf: string;
+  password: string;
+}
+
+export interface UserDocument extends User, mongoose.Document {}
+
+const userSchema: mongoose.Schema<UserDocument> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [3, "Too short name."],
+  },
+  cpf: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: isValidCPF,
+      message: "Invalid CPF",
+    },
+    required: true,
+    minlength: [11, "Too short cpf."],
+    maxlength: [11, "Too long cpf."],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Too short password."],
+  },
+});
+
+export const userModel = mongoose.model<UserDocument>("User", userSchema);
+
+function isValidCPF(cpf: string): boolean {
+  let remainder: number;
+  let digitsSum = 0;
+
+  if (cpf.length !== 11 || cpf == "00000000000") return false;
+
+  for (let i = 1; i <= 9; i++) digitsSum = digitsSum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  remainder = (digitsSum * 10) % 11;
+
+  if (remainder == 10 || remainder == 11) remainder = 0;
+  if (remainder != parseInt(cpf.substring(9, 10))) return false;
+
+  digitsSum = 0;
+  for (let i = 1; i <= 10; i++) digitsSum = digitsSum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  remainder = (digitsSum * 10) % 11;
+
+  if (remainder == 10 || remainder == 11) remainder = 0;
+  if (remainder != parseInt(cpf.substring(10, 11))) return false;
+  return true;
+}
