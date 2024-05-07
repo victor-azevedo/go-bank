4afd1a783db1bafb8434b313ace897d519f70c85
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-07 09:17:48 -0300

    feat: add unprocessable entity error

---
+++ src/errors/unprocessable-entity-error.ts
@@ -0,0 +1,8 @@
+import httpStatus from "http-status";
+import { ApplicationError } from "./application-error";
+
+export class UnprocessableEntityError extends ApplicationError {
+  constructor({ message = "Unprocessable Entity", details }: { message?: string; details?: string[] }) {
+    super({ message, statusCode: httpStatus.BAD_REQUEST, details });
+  }
+}
--- src/middlewares/request-validation-middleware.ts
+++ src/middlewares/request-validation-middleware.ts
@@ -1,27 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import httpStatus from "http-status";
 import { AnySchema } from "joi";
+import { UnprocessableEntityError } from "../errors";
 
 export function validateBody<T>(schema: AnySchema<T>) {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body, { abortEarly: false, convert: false });
 
-    if (!error) {
-      next();
-    } else {
-      return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(invalidDataError(error.details.map((d) => d.message)));
-    }
-  };
-}
+    if (error) throw new UnprocessableEntityError({ details: error.details.map((d) => d.message) });
 
-interface InvalidDataError {
-  message: string;
-  details: string[];
-}
-
-function invalidDataError(details: string[]): InvalidDataError {
-  return {
-    message: "Invalid data",
-    details,
+    next();
   };
 }
