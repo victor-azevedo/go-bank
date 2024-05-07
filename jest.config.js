9868941623ff48c4671d49059b844d80f598c8ff
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-06 21:26:28 -0300

    test: add envs-test-config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     ".+\\.ts$": "ts-jest",
   },
   testMatch: ["<rootDir>/tests/**/*.(test|spec).ts"],
-  setupFiles: ["<rootDir>/tests/setup-envs.ts"],
+  setupFiles: ["<rootDir>/tests/envs-tests-config.ts"],
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
     "@test/(.*)": "<rootDir>/tests/$1",
