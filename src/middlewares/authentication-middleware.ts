e502be04103e22db66bceb8bfbdff58bbdabbf60
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-07 12:02:12 -0300

    refactor: remove async and error message

--- src/middlewares/authentication-middleware.ts
+++ src/middlewares/authentication-middleware.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Response } from "express";
 import * as jwt from "jsonwebtoken";
+import { jwtConfig } from "../config";
 import { UnauthorizedError } from "../errors/";
 import { ApplicationRequest } from "../interfaces/";
-import { jwtConfig } from "../config";
 
-export async function authenticateToken(req: ApplicationRequest, _res: Response, next: NextFunction) {
+export function authenticateToken(req: ApplicationRequest, _res: Response, next: NextFunction) {
   const authHeader = req.header("Authorization");
-  if (!authHeader) throw new UnauthorizedError("Bearer token not provided");
+  if (!authHeader) throw new UnauthorizedError();
 
   const token = authHeader.split(" ")[1];
-  if (!token) throw new UnauthorizedError("Bearer token not provided");
+  if (!token) throw new UnauthorizedError();
 
   try {
     const { userId } = jwt.verify(token, jwtConfig.secretKey!) as jwt.JwtPayload;
