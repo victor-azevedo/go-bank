09cb3fb6d7a35acf8094621b97bf9078a88dbe87
Victor Azevedo <josevcazevedo@gmail.com>
2024-05-07 20:40:40 -0300

    doc: generic swagger autogen

--- src/swagger.ts
+++ src/swagger.ts
@@ -36,11 +36,11 @@ const endpointsFiles = [
 
 const options = {
   openapi: "3.0.0",
-  // language:         //<string>,     Change response language.                      By default is 'en-US'
-  // disableLogs:      //<boolean>,    Enable/Disable logs.                           By default is false
-  autoHeaders: false, //<boolean>,    Enable/Disable automatic headers recognition.  By default is true
-  autoQuery: false, //<boolean>,    Enable/Disable automatic query recognition.    By default is true
-  autoBody: false, //<boolean>,    Enable/Disable automatic body recognition.     By default is true
-  writeOutputFile: true, //<boolean>     Enable/Disable writing the output file.        By default is true
+  // language:          //<string>,     Change response language.                      By default is 'en-US'
+  // disableLogs:       //<boolean>,    Enable/Disable logs.                           By default is false
+  // autoHeaders:       //<boolean>,    Enable/Disable automatic headers recognition.  By default is true
+  // autoQuery:         //<boolean>,    Enable/Disable automatic query recognition.    By default is true
+  // autoBody:          //<boolean>,    Enable/Disable automatic body recognition.     By default is true
+  writeOutputFile: false, //<boolean>     Enable/Disable writing the output file.        By default is true
 };
 swaggerAutogen(options)(outputFile, endpointsFiles, doc);
